8d8715af3531105bd8a7906e9d2dc2b1f615cbca
ducaifeng86 <1613195189@qq.com>
2020-01-13 18:07:50 +0800

    header的实现

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Home from './pages/home/Home.vue'
+export default {
+  name: 'app',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="header">
+		<div class="header-left">
+			<div class="iconfont back-icon">&#xe892;</div>
+		</div>
+		<div class="header-input">
+			<span class="iconfont">&#xe609;</span>
+			输入城市/景点/游玩景点
+		</div>
+		<div class="header-right">城市
+			<span class="iconfont">&#xe6aa;</span>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'HomeHeader'
+	}
+</script>
+
+<style lang='stylus' scoped>
+	.header
+		display:flex;
+		line-height:.86rem;
+		background:#00bcd4;
+		color:#fff;
+		.header-left
+			width:.64rem;
+			float:left;
+			.back-icon
+				font-size:.4rem
+		.header-input
+			flex:1;
+			margin-top:.12rem;
+			margin-left:.12rem
+			height: .62rem
+			line-height:.62rem
+			background:#fff
+			border-radius:.05rem
+			color:#ccc
+		.header-right
+			width:1.24rem;
+			float:right;
+			text-align: center;
+</style>
