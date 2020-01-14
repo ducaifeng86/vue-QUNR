c5bc276fcbcf53ce73df33dcf327c68642081416
ducaifeng86 <1613195189@qq.com>
2020-01-14 11:18:08 +0800

    添加首页轮播图

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/styles/reset.css'
 import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   render: h => h(App),
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption">
+	    <!-- slides -->
+	    <swiper-slide v-for="item in swiperList" :key="item.id">
+	    	<img :src="item.imgUrl" class="swiper-img"/>
+	    </swiper-slide>
+	    <!-- Optional controls -->
+	    <div class="swiper-pagination"  slot="pagination"></div>
+	  </swiper>
+  </div>
+</template>
+
+<script>
+	export default {
+		name:'HomeSwiper',
+		data(){
+			return {
+				swiperOption:{
+					pagination:'.swiper-pagination',
+					loop:true
+				},
+				swiperList:[
+					{
+						id:'001',
+						imgUrl:require('../../../assets/imgs/2.jpg')
+					},
+					{
+						id:'002',
+						imgUrl:require('../../../assets/imgs/2.jpg')
+					},
+					{
+						id:'003',
+						imgUrl:require('../../../assets/imgs/3.jpg')
+					}
+				]
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+	.wrapper >>> .swiper-pagination-bullet-active
+	    background: #fff !important
+	.wrapper
+		overflow:hidden
+		width:100%
+		height:0
+		padding-bottom:30.48%
+		background:#ccc
+	.swiper-img
+		width:100%
+</style>
